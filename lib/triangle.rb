b5635f917febd225132535b58bdc9b1803f94bc0
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-12 20:26:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   end
 
 def kind
-  @triangle.each do |x| 
+  @triangle.map do |x| 
  return :poop if x.any? <= 0 or x[0] + x[1] <= x[2]
   return :equilateral if x[0] == x[2]
   return :isosceles if x[0] == x[1] or x[1] == x[2]
