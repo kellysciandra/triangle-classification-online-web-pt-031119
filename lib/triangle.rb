625ae8b88f6214c8e76bf82b4445c737729d8493
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-12 20:36:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,11 +13,12 @@ class Triangle
         :isosceles
       elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
         :scalene
+        binding.pry
       elsif (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
       else (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
-      binding.pry
+      
       end
     end
   end
