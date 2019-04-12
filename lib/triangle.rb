4931cd8ad3ed8bd04bad133d825a45be82b5919b
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-12 20:30:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,9 @@ class Triangle
   end
 
   def kind()
+    binding.pry
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-      binding.pry
+      
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
