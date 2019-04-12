0137c4f37cb19d3da644a4168e3bf6ca31da6cb1
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-12 20:32:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,6 @@ class Triangle
   end
 
   def kind()
-    if 
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
       elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
