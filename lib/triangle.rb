cbff7e76af395388d9e0f9dcf3f14f8b7bb0cb17
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-12 20:30:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Triangle
   def initialize(side_1,side_2,side_3)
     @side_1 = side_1
@@ -7,6 +9,7 @@ class Triangle
 
   def kind()
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      binding.pry
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
