4e2bb1615fa2a2786a012ffa6aca89b3ce4659d8
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-12 20:59:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,6 @@ class Triangle
      if (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
           raise TriangleError
         end 
-        
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
       elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
