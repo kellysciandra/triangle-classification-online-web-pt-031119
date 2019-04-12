f93172b07947ce0614c6ab67a2cf1efe5a9f400a
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-12 20:02:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  def initialize(side_1, side_2, side_3)
+    @triangle_sides = []
+    @triangle_sides << side_1
+    @triangle_sides << side_2
+    @triangle_sides << side_3
+     
+  end
+
+def kind
+  @triangle_sides.each do |x|
+  if ((x[1] == x[2]) || (x[1] == x[3]) || (x[2] == x[3]))
+    return :equilateral
+  elsif ((x[1] == x[2]) || (x[1] == x[3]) || (x[2] == x[3]))
+    return :isosceles
+  elsif
+     :scalene
+    else
+      raise TriangleError
+   
+    end 
+  end
 end
+
+    
+
+
+
+class TriangleError < StandardError
+
+end 
+end 
